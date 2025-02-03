405c7b3adedad3884ba86c632137335a0e3d5180
Piqulsky <piqulsky@gmail.com>
2025-02-03 18:46:26 +0100

    Decrementing buttons

--- buasgamejam/Scripts/BattleScene.gd
+++ buasgamejam/Scripts/BattleScene.gd
@@ -3,6 +3,9 @@ extends Node2D
 var building_scene = preload("res://Scenes/building.tscn")
 var removing_scene = preload("res://Scenes/remove_sprite.tscn")
 
+@onready var mouse_point := $MousePoint
+@onready var buildable_area := $BuildableArea
+
 var selected_object :CollisionObject2D
 @onready var occupied_coordinates := [$HeartStatic.position]
 
@@ -10,17 +13,31 @@ var planning := true
 
 enum {ADDING, DELETING}
 var current_mode := ADDING
+var current_building_key := "Building"
+var current_button :TextureButton
+
+var buildingsLeft := {
+	"Building": 15,
+	"Wall": 5,
+	"Turret": 2
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	selected_object = building_scene.instantiate()
 	add_child(selected_object)
+	for key in buildingsLeft.keys():
+		var button = get_node("BattleHUD/PlanningButtonsPanel/" + key + "Button") as TextureButton
+		button.pressed.connect(_on_building_button_pressed.bind("key", key))
+		var buttonText = button.get_child(0) as Label
+		buttonText.text = str(buildingsLeft[key])
+	current_button = get_node("BattleHUD/PlanningButtonsPanel/" + current_building_key + "Button") as TextureButton
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$MousePoint.position = get_local_mouse_position()
-	if planning and $BuildableArea.overlaps_body($MousePoint):
+	mouse_point.position = get_local_mouse_position()
+	if planning and buildable_area.overlaps_body(mouse_point):
 		var mouseX = get_local_mouse_position().x
 		var mouseY = get_local_mouse_position().y
 		var newPos = Vector2(snapped(mouseX, 32), snapped(mouseY, 32))
@@ -29,6 +46,8 @@ func _process(delta: float) -> void:
 				if newPos not in occupied_coordinates:
 					selected_object.position = newPos
 				if Input.is_action_just_pressed("confirm"):
+					buildingsLeft[current_building_key] -= 1
+					(current_button.get_child(0) as Label).text = str(buildingsLeft[current_building_key])
 					selected_object.collision_layer = 1
 					occupied_coordinates.append(selected_object.position)
 					selected_object.add_to_group("buildings")
@@ -44,7 +63,8 @@ func _process(delta: float) -> void:
 							chosen.queue_free()
 
 
-func _on_wall_button_pressed() -> void:
+func _on_building_button_pressed(key: String) -> void:
+	current_building_key = key
 	selected_object.queue_free()
 	selected_object = building_scene.instantiate()
 	add_child(selected_object)
