ca6989765dafdebc22795e6f94c300f2d37fcb1b
Piqulsky <piqulsky@gmail.com>
2025-02-03 11:17:05 +0100

    Basic enemy and heart of the keep

---
+++ buasgamejam/Scripts/Enemy.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+var target :Vector2
+var bounceVector :Vector2
+
+func _ready() -> void:
+	var heart := $"../HeartStatic"
+	target = heart.position
+
+
+func _physics_process(delta: float) -> void:
+	if target:
+		var direction = position.direction_to(target) + bounceVector
+		bounceVector = Vector2(move_toward(bounceVector.x, 0, delta), move_toward(bounceVector.y, 0, delta))
+		velocity = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.y, 0, SPEED)
+
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		var collidedBuilding := collision_info.get_collider() as Building
+		collidedBuilding.hit()
+		bounceVector = collision_info.get_normal() * 2
---
+++ buasgamejam/Scripts/Heart.gd
@@ -0,0 +1,7 @@
+extends Building
+
+class_name Heart
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	life = 3
